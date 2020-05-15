8b2823421b07562dd27436f5d0789d74707bdd5c
Aarav Sharma <aaravlikestennis@gmail.com>
2020-05-15 15:25:13 -0700

    VictorSPX Subsystem

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,23 +1,31 @@
 package frc.robot.subsystems.intake;
 
+import javax.swing.text.ViewFactory;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
- * For your first code assignment, you're going to be writing a subsystem 
- * for the intake on our 2020 robot. There's a tutorial on Notion that's going 
- * to step you through 
+ * For your first code assignment, you're going to be writing a subsystem for
+ * the intake on our 2020 robot. There's a tutorial on Notion that's going to
+ * step you through
  */
 public class IntakeSub extends SubsystemBase {
   // Aarav Sharma's Comment
   /**
    * Creates a new IntakeSub.
    */
-  public IntakeSub() {
+  private VictorSPX victor;
 
+  public IntakeSub(VictorSPX victor) {
+    this.victor = victor;
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+public boolean getInverted() {
+  boolean victor = false;
+  return victor;
+}
+
 }
