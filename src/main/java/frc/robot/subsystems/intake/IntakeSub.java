01be42c582280d2083405abc268071e3c7b7dd20
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2020-05-15 15:27:33 -0700

    I tried importing VictorSPX, doesn’t work

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,22 +1,41 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
- * For your first code assignment, you're going to be writing a subsystem 
- * for the intake on our 2020 robot. There's a tutorial on Notion that's going 
- * to step you through 
+ * For your first code assignment, you're going to be writing a subsystem for
+ * the intake on our 2020 robot. There's a tutorial on Notion that's going to
+ * step you through
  */
 public class IntakeSub extends SubsystemBase {
   /**
    * Creates a new IntakeSub.
    */
-  public IntakeSub() {
+  private VictorSPX victor;
 
+  
+  public VictorSPX getVictor(){
+    return victor.setInverted();
+  }
+  public IntakeSub(VictorSPX victor) {
+    this.victor = victor;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  //power function: I am still working on this
+  // public void setPower(double power){
+	// 	//Object intakeVictor;
+  //   //Object ControlMode;
+  //   intakeVictor.set(ControlMode.PercentOutput, power);
+  // }
+  
+  // public void setPower(double power){
+	// 	Object intakeVictor;
+  //   intakeVictor.set(ControlMode.PercentOutput, power);
+	// }
 }
