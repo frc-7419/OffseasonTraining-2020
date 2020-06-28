e1275e6682e1af97dbebcdf856447e5c8bb017b7
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2020-06-27 22:06:38 -0700

    I got my build to be successful for the week 2 assignment

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,26 +1,52 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.VictorSPX;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.team7419.Initers;
 
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.team7419.Initers;
+import javax.swing.text.ViewFactory;
 /**
  * For your first code assignment, you're going to be writing a subsystem for
  * the intake on our 2020 robot. There's a tutorial on Notion that's going to
  * step you through
  */
 public class IntakeSub extends SubsystemBase {
-  /**
+  //private static final VictorSPX VictorSPX = null;
+/**
    * Creates a new IntakeSub.
    */
   private VictorSPX victor;
 
   
-  public VictorSPX getVictor(){
-    return victor.setInverted();
-  }
   public IntakeSub(VictorSPX victor) {
     this.victor = victor;
+    Initers.initVictors(victor);
+    //int init = Initers.initVictors(VictorSPX victor);
+  }
+
+  public VictorSPX getVictor(){
+    return victor;
+    
+  }
+  // public IntakeSub(VictorSPX victor) {
+  //   this.victor = victor;
+  // }
+  // public IntakeSub(){
+
+  // }
+
+  // public IntakeSub(VictorSPX victor) {
+  //   this.victor = victor;
+  // }
+
+  //public IntakeSub() {
+
+  //}
+
+  public IntakeSub() {
+
   }
 
   @Override
@@ -33,6 +59,12 @@ public class IntakeSub extends SubsystemBase {
   //   //Object ControlMode;
   //   intakeVictor.set(ControlMode.PercentOutput, power);
   // }
+
+  public boolean getInverted() {
+    return false;
+    //return victor.getInverted();
+  }
+
   
   // public void setPower(double power){
 	// 	Object intakeVictor;
