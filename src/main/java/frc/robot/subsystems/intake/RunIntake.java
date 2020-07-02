53d278a32ba99bf0b9b58aac579cf670b30855c2
Samarth Shah <samarthandroid145@gmail.com>
2020-07-02 15:55:08 -0700

    public void end method completed for week 3

--- src/main/java/frc/robot/subsystems/intake/RunIntake.java
+++ src/main/java/frc/robot/subsystems/intake/RunIntake.java
@@ -27,6 +27,7 @@ public class RunIntake extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    intake.setPower(0.0);
   }
 
   // Returns true when the command should end.
