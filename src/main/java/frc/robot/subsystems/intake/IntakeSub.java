44df5abf179dfdfa7593abfea720394d1c6cd468
Kushal Kodnad <kkodnad@gmail.com>
2020-07-02 15:41:17 -0700

    week 2 assignment edits

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.intake;
-
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.team7419.Initers;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 // hello - kushal
 /**
  * For your first code assignment, you're going to be writing a subsystem 
@@ -11,12 +14,23 @@ public class IntakeSub extends SubsystemBase {
   /**
    * Creates a new IntakeSub.
    */
-  public IntakeSub() {
+  private VictorSPX victor;
 
+  public IntakeSub(VictorSPX victor) {
+    this.victor = victor;
+    Initers.initVictors(victor);
+  }
+
+  public boolean getInverted(){
+    return victor.getInverted(); //idk if this is right??
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  /*public void setPower(double power){
+		intakeVictor.set(ControlMode.PercentOutput, power);
+	}*/
 }
