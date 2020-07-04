77ad3b1e04451e4fa8bece92384eb74e91ccf9c4
H3nryM <38223629+H3nryM@users.noreply.github.com>
2020-07-04 15:08:47 -0700

    Revert "getting week 3 test"

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.team7419.Initers;
+
+//added a comment to intake sub
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
@@ -11,13 +16,21 @@ public class IntakeSub extends SubsystemBase {
   /**
    * Creates a new IntakeSub.
    */
-  public IntakeSub() {
+  private VictorSPX victor;
 
+  public IntakeSub(VictorSPX victor) {
+      this.victor = victor;
+      Initers.initVictors(victor);
+      victor.setInverted(false);
   }
-
+  public boolean getInverted(){
+    return victor.getInverted();
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-}
-
+  public void setPower(double power){
+    victor.set(ControlMode.PercentOutput, power);
+  }
+} 
