2b16baa675fc778a9fbe31091770ffeb74bdba9d
Samarth Shah <samarthandroid145@gmail.com>
2020-07-09 15:37:35 -0700

    changes with factory not working

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,18 +1,34 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.team7419.PaddedXbox;
+
+import frc.robot.Constants.CanIds;
+import frc.robot.subsystems.intake.*;
+
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here. 
+ * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
 
-  /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
+  private VictorSPX intakeVictor = new VictorSPX(CanIds.intakeVictor.id);
+
+  // The robot's subsystems are defined here
+  private final IntakeSub intake = new IntakeSub(intakeVictor);
+
+  // The commands that run on those subsystems are defined here
+  private final RunIntake runIntake = new RunIntake(intake);
+
+  private final PaddedXbox controller = new PaddedXbox();
+  
   public RobotContainer() {
+    this.factory = factory;
     // Configure the button bindings
+    runIntake = factory.getIntakeSub();
+    controller = factory.getPaddedXbox();
     configureButtonBindings();
   }
 
@@ -21,5 +37,9 @@ public class RobotContainer {
    * We're going to teach you how to use this later.
    */
   private void configureButtonBindings() {
+
+    controller.getA();
+    factory.getRunIntake(0.5);
+
   }
 }
