3e00320e1c59b945e799b8abc30b82b7b1862175
Aarav Wattal <homerun2023@gmaill.com>
2020-07-09 15:02:09 -0700

    Error Message

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -25,3 +25,6 @@ public class IntakeSub extends SubsystemBase {
     victor.set(ControlMode.PercentOutput, power);
   }
 }
+
+// ERROR
+// ENOENT: no such file or directory, stat '/Users/aaravwattal/FRCOffseasonTraining/src/main/java/frc/robot/subsystems/intake/gradlew'
