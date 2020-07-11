2a95d3af9f88e674e1f5e75ac1a4bd10ccf619c9
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2020-07-11 14:15:49 -0700

    Week 4 Robot Container Merge Conflicts

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,41 +1,18 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.team7419.PaddedXbox;
-
-import frc.robot.Constants.CanIds;
-import frc.robot.subsystems.intake.*;
-
-
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
- 
+ * (including subsystems, commands, and button mappings) should be declared here. 
  */
 public class RobotContainer {
 
-  private VictorSPX intakeVictor = new VictorSPX(CanIds.intakeVictor.id);
-
-  // The robot's subsystems are defined here
-  private final IntakeSub intake = new IntakeSub(intakeVictor);
-
-  // The commands that run on those subsystems are defined here
-  private final RunIntake runIntake = new RunIntake();
-  //private Factory factory;
-  private IntakeSub intakeSub;
-  private PaddedXbox controller;
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
-  
-
-  //private Object factory;
-
   public RobotContainer() {
     // Configure the button bindings
-    
     configureButtonBindings();
   }
 
