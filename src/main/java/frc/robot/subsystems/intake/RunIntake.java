e8bdec9038fe0708a4ed33f691782ecfe67324c3
Harini <harinib246@gmail.com>
2020-07-22 15:09:08 -0700

    commiting

--- src/main/java/frc/robot/subsystems/intake/RunIntake.java
+++ src/main/java/frc/robot/subsystems/intake/RunIntake.java
@@ -3,13 +3,16 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class RunIntake extends CommandBase {
+  private IntakeSub intakesub;
+  private double power;
   /**
    * Creates a new RunIntake.
    */
-  public RunIntake() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  public RunIntake(IntakeSub intake, double power) {
+  // Use addRequirements() here to declare subsystem dependencies.
+    this.intakesub = intake;
+    this.power = power;
   }
-
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
@@ -18,11 +21,13 @@ public class RunIntake extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    intakesub.setPower(power);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    intakesub.setPower(0);
   }
 
   // Returns true when the command should end.
