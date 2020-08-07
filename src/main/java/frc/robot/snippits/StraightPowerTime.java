cd60f157badefc9fb08a8912c4f716f9296c32b6
Aarav Wattal <homerun2023@gmaill.com>
2020-08-07 00:41:37 -0700

    just a commit before i mess with the isFinished function

--- src/main/java/frc/robot/snippits/StraightPowerTime.java
+++ src/main/java/frc/robot/snippits/StraightPowerTime.java
@@ -1,14 +1,22 @@
 package frc.robot.snippits;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivebase.DriveBaseSub;
 
 public class StraightPowerTime extends CommandBase {
 
+  private DriveBaseSub driveBaseSub;
+  private double power; // double because power is double
+  private int time; // int because milliseconds
+  
 
   /**
    * Runs the drive base straight at a power for a time
    */
-  public StraightPowerTime() {
+  public StraightPowerTime(DriveBaseSub driveBaseSub, double power, int time) {
+    this.driveBaseSub = driveBaseSub;
+    this.power = power;
+    this.time = time;
   }
 
   @Override
@@ -17,11 +25,13 @@ public class StraightPowerTime extends CommandBase {
 
   @Override
   public void execute() {
+    driveBaseSub.setPower(power);
   }
 
 
   @Override
   public void end(boolean interrupted) {
+    driveBaseSub.setPower(0);
   }
 
   @Override
