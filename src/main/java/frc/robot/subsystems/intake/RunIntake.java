32d55b2ade543b84d301b343128a1944ff9e7e8a
Karan Saini <karansaini117@gmail.com>
2020-08-27 18:20:03 -0700

    week 3 done!

--- src/main/java/frc/robot/subsystems/intake/RunIntake.java
+++ src/main/java/frc/robot/subsystems/intake/RunIntake.java
@@ -3,26 +3,34 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class RunIntake extends CommandBase {
+  private IntakeSub intake;
+  private double power;
   /**
    * Creates a new RunIntake.
    */
-  public RunIntake() {
+  
+  public RunIntake(IntakeSub intake, double power) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.intake = intake;
+    this.power = power;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    intake.setPower(power);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    intake.setPower(0);
   }
 
   // Returns true when the command should end.
