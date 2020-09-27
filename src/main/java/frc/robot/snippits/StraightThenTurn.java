130d1b066212a5de2d55c0fee6451318976261dd
Karan Saini <karansaini117@gmail.com>
2020-09-27 16:41:28 -0700

    completed week 10 assignment

--- src/main/java/frc/robot/snippits/StraightThenTurn.java
+++ src/main/java/frc/robot/snippits/StraightThenTurn.java
@@ -1,16 +1,21 @@
 package frc.robot.snippits;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drivebase.DriveBaseSub;
 
 public class StraightThenTurn extends SequentialCommandGroup {
-
 
   /**
    * Creates a new StraightThenTurn.
    */
-  public StraightThenTurn() {
+
+  public StraightThenTurn(TalonFX leftMast, TalonFX rightMast, TalonFX leftFollow, TalonFX rightFollow) {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
-    super();
+    super(new StraightPowerTime(new DriveBaseSub(leftMast, rightMast, leftFollow, rightFollow), 0.5, 1000), new TurnPowerTime(new DriveBaseSub(leftMast, rightMast, leftFollow, rightFollow), "LEFT", 0.5, 1000));
+
+    
   }
 }
