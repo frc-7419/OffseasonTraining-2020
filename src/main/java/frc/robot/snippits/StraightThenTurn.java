2d521b6fc92ebc69dfd44cb632f58da028abc7ba
Dhruv Bisla <bisladhruv@gmail.com>
2020-11-23 15:44:56 -0800

    Complete week 10 assignments

--- src/main/java/frc/robot/snippits/StraightThenTurn.java
+++ src/main/java/frc/robot/snippits/StraightThenTurn.java
@@ -1,6 +1,7 @@
 package frc.robot.snippits;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drivebase.DriveBaseSub;
 
 public class StraightThenTurn extends SequentialCommandGroup {
 
@@ -8,9 +9,10 @@ public class StraightThenTurn extends SequentialCommandGroup {
   /**
    * Creates a new StraightThenTurn.
    */
-  public StraightThenTurn() {
+
+  public StraightThenTurn(DriveBaseSub driveBase, double straightPower, double straightTime, String turnDirection, double turnPower, double turnTime) {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
-    super();
+    super(new StraightPowerTime(driveBase, straightPower, straightTime), new TurnPowerTime(driveBase, turnDirection, turnPower, turnTime));
   }
 }
