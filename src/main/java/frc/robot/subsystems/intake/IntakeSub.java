085e18f78197d5c49d9de3202fe891df4f1dee06
pryme-svg <edoc.www@gmail.com>
2021-09-15 16:49:59 -0700

    finish week 3

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.team7419.Initers;
 
@@ -25,6 +26,9 @@ public class IntakeSub extends SubsystemBase {
   }
   public boolean getInverted() {
     return this.victor.getInverted();
+  }
+  public void setPower(double power) {
+    victor.set(ControlMode.PercentOutput, power);
   }
 
   @Override
