2662c53e0045d6b453e7a77a81875fe01fa83798
pryme-svg <edoc.www@gmail.com>
2021-09-24 08:41:50 -0700

    week 10
    no test?

--- src/main/java/frc/robot/snippits/StraightThenTurn.java
+++ src/main/java/frc/robot/snippits/StraightThenTurn.java
@@ -1,6 +1,7 @@
 package frc.robot.snippits;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drivebase.DriveBaseSub;
 
 public class StraightThenTurn extends SequentialCommandGroup {
 
@@ -8,9 +9,9 @@ public class StraightThenTurn extends SequentialCommandGroup {
   /**
    * Creates a new StraightThenTurn.
    */
-  public StraightThenTurn() {
+  public StraightThenTurn(DriveBaseSub driveBase, double spower, double stime, String direction, double tpower, double ttime) {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
-    super();
+    super(new StraightPowerTime(driveBase, spower, stime), new TurnPowerTime(driveBase, direction, tpower, ttime));
   }
 }
