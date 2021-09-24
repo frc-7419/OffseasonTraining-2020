a128aa5a1ad3a0417764c5e71219d5b6857e1385
pryme-svg <edoc.www@gmail.com>
2021-09-23 18:44:55 -0700

    week 7

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import com.team7419.PaddedXbox;
 
+import frc.robot.subsystems.drivebase.ArcadeDrive;
 import frc.robot.subsystems.drivebase.DriveBaseSub;
 import frc.robot.subsystems.intake.IntakeSub;
 
@@ -16,6 +17,7 @@ public class RobotContainer {
   private IntakeSub intake;
   private PaddedXbox xbox;
   private DriveBaseSub driveBaseSub;
+  private ArcadeDrive arcadeDrive;
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -39,6 +41,8 @@ public class RobotContainer {
 
   public void setDefaultCommands() {
     this.intake.setDefaultCommand(factory.getRunIntakeWithJoystick(this.xbox));
+    // this.driveBaseSub.setDefaultCommand(factory.getArcadeDrive(this.xbox));
     this.driveBaseSub.setDefaultCommand(factory.getTankDrive(this.xbox));
+
   }
 }
--- src/main/java/frc/robot/subsystems/drivebase/ArcadeDrive.java
+++ src/main/java/frc/robot/subsystems/drivebase/ArcadeDrive.java
@@ -12,14 +12,15 @@ public class ArcadeDrive extends CommandBase {
    */
   private DriveBaseSub driveBaseSub;
   private PaddedXbox xbox;
-  private double a;
-  private double b;
+  private double straight;
+  private double turn;
 
-  public ArcadeDrive(DriveBaseSub driveBaseSub, PaddedXbox xbox, double a, double b) {
+  public ArcadeDrive(DriveBaseSub driveBaseSub, PaddedXbox xbox, double straight, double turn) {
     this.driveBaseSub = driveBaseSub;
     this.xbox = xbox;
-    this.a = a;
-    this.b = b;
+    this.straight = straight;
+    this.turn = turn;
+    addRequirements(driveBaseSub);
   }
 
   @Override
@@ -28,10 +29,22 @@ public class ArcadeDrive extends CommandBase {
 
   @Override
   public void execute() {
+    double a = xbox.getLeftY() * straight;
+    double b = xbox.getRightX() * turn;
+
+    this.driveBaseSub.setPowerLeftMast(a);
+    this.driveBaseSub.setPowerLeftFollow(a);
+
+    this.driveBaseSub.setPowerRightMast(b);
+    this.driveBaseSub.setPowerRightFollow(b);
   }
 
   @Override
   public void end(boolean interrupted) {
+    this.driveBaseSub.setPowerLeftMast(0);
+    this.driveBaseSub.setPowerRightMast(0);
+    this.driveBaseSub.setPowerLeftFollow(0);
+    this.driveBaseSub.setPowerRightFollow(0);
   }
 
   @Override
