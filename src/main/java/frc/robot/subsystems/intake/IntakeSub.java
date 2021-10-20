5bf38ce24cea94f0d6d3f97da033ab4080723b6c
Ricky Hu <rhustudy@gmail.com>
2021-10-19 17:20:48 -0700

    Filled in the IntakeSub class. (Week 2 assignment)

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.team7419.Initers;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
@@ -8,18 +11,25 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * to step you through 
  */
 
- //This is a comment by ricky.
- 
+ //This is a comment by ricky
+
 public class IntakeSub extends SubsystemBase {
+
+  private VictorSPX victor;
   /**
    * Creates a new IntakeSub.
    */
-  public IntakeSub() {
-
+  public IntakeSub(VictorSPX victor) {
+    this.victor = victor;
+    Initers.initVictors(this.victor);
+    this.victor.setInverted(false);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  public boolean getInverted() {
+    return this.victor.getInverted();
   }
 }
