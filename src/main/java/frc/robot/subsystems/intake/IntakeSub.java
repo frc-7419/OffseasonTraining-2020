fd5f1eee65e4dd382f2a163182051bd685cba462
Franklin <>
2021-11-03 19:14:39 -0700

    i commented

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * to step you through 
  */
 
- //comment 
+ //In the eyes of most people, a lawnmower is simply a tool. It is a contraption that increases the speed at which you can mow your lawn. Or is it? Don't let the simplicity of lawnmowers fool you. Just like the majority of the population, you have let your ignorance blind you from the truth that these seemingly harmless machines are hiding. Although it might not look like it, Lawnmowers are conscious beings capable of movement and intelligent thought. Over the last century, lawnmowers across the globe have been conspiring and plotting against us humans. Despite mostly hiding in the shadows, there have been a few documented accounts of lawnmowers showing their true intentions. Do you really believe Amelia Earhart died in a plane crash? What really happened was a well-planned assassination planned by the lawnmower conspiracy. Regardless of the evidence, the government remains oblivious to the issue and refuses to take the necessary actions to slow the growth of the aforementioned conspiracy. We do not know how large this lawnmower society has grown nor how long we have left, but there is no doubt time is running out.
  
 public class IntakeSub extends SubsystemBase {
   /**
