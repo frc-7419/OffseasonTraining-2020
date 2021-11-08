262ff33074fb4a2e1e02ad0c6d891977dd7726fc
poluk sharma <poluk.sharma@gmail.com>
2021-11-07 20:54:48 -0800

    Week 2 and 3 Assignments

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,27 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.team7419.Initers;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/**
- * For your first code assignment, you're going to be writing a subsystem 
- * for the intake on our 2020 robot. There's a tutorial on Notion that's going 
- * to step you through 
- */
-
- //comment 
- //Installed VS Code Comment
- //comment
- 
 public class IntakeSub extends SubsystemBase {
-  /**
-   * Creates a new IntakeSub.
-   */
-  public IntakeSub() {
-
+  /** Creates a new IntakeSub. */
+  
+private VictorSPX victor;
+  
+public IntakeSub(VictorSPX victor) {
+  this.victor = victor;
+  Initers.initVictors(victor);
+  victor.setInverted(false);
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public boolean getInverted() {
+    return victor.getInverted();
+  }
+
+  public void setPower(double power) {
+    victor.set(ControlMode.PercentOutput, power);
+  }
+
+  public VictorSPX getVictor() {
+    return this.victor;
   }
 }
