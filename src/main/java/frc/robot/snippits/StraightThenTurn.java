9dc615ce90f5141a991acdf0f6cc598ade7ce6c6
jinnie20 <ktakwu@gmail.com>
2021-11-28 20:02:54 -0800

    week 10 :)

--- src/main/java/frc/robot/snippits/StraightThenTurn.java
+++ src/main/java/frc/robot/snippits/StraightThenTurn.java
@@ -9,8 +9,11 @@ public class StraightThenTurn extends SequentialCommandGroup {
    * Creates a new StraightThenTurn.
    */
   public StraightThenTurn() {
+    public StraightThenTurn(DriveBaseSub driveBase, double straightPower, double straightTime, String direction, double turnPower, double turnTime){}
+  }
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
     super();
+    super(new StraightPowerTime(driveBase, straightPower, straightTime), new TurnPowerTime(driveBase, direction, turnPower, turnTime ));
   }
 }
