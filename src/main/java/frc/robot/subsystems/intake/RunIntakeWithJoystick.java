6bfbaab3e4df4c2cdf8855fc3dc958f5fab8d623
Keshav Badrinath <keshavbadrinath06@gmail.com>
2021-12-07 09:55:33 -0800

    week 5 finished again

---
+++ src/main/java/frc/robot/subsystems/intake/RunIntakeWithJoystick.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+import com.team7419.PaddedXbox;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+public class RunIntakeWithJoystick extends CommandBase {
+  private IntakeSub intake;
+  private PaddedXbox joystick;
+  /** Creates a new RunIntakeWithJoystick. */
+  public RunIntakeWithJoystick(IntakeSub intakeSub, PaddedXbox joystick) {
+   
+    this.intake = intakeSub;
+    this.joystick = joystick;
+    addRequirements(intake);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intake.setPower(joystick.getLeftY());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.setPower(0);
+  }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
