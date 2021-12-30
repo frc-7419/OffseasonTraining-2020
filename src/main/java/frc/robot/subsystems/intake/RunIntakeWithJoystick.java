62649c3d3a9dd8d715b76467b7f4be5976db1a07
Kaival145 <kaival.s.shah@gmail.com>
2021-12-29 19:38:24 -0800

    adding file

---
+++ src/main/java/frc/robot/subsystems/intake/RunIntakeWithJoystick.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+import com.team7419.PaddedXbox;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class RunIntakeWithJoystick extends CommandBase {
+  
+  private IntakeSub intake;
+  private PaddedXbox joystick;
+  /** Creates a new RunIntakeWithJoystick. */
+  public RunIntakeWithJoystick(IntakeSub intake, PaddedXbox joystick) {
+    this.intake = intake;
+    this.joystick = joystick;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  
+  // changing speed based on joystick input from PaddedXbox --> joystick
+  @Override 
+  public void execute() {
+    this.intake.setPower(joystick.getLeftY());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    this.intake.setPower(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
