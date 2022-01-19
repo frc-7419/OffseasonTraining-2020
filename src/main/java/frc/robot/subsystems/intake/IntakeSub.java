6e595c73fb0ed7158089e2ca1174dc1f74caaba9
braden0m <95321149+braden0m@users.noreply.github.com>
2022-01-18 16:48:09 -0800

    Week 1 Github commit

--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorocontrol.can.VictorSPX;
+import com.team7419.Initers;
 
 /**
  * For your first code assignment, you're going to be writing a subsystem 
@@ -9,17 +11,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 
  //stephen was here 
+ //telebop
  
 public class IntakeSub extends SubsystemBase {
+  private VictorSPX victor1;
   /**
    * Creates a new  IntakeSub.
    */
-  public IntakeSub() {
-
+  public IntakeSub(VictorSPX victor1) {
+    victor1 = victor1;
+    Initiers.initVictors(victor1);
+    victor1.setInverted();
+  }
+  public String getInverted() {
+    return victor1.getInverted();
   }
 
-  @Override
+
+  @Override  
   public void periodic() {
     // This method will be called once per scheduler run
+    
   }
 }
