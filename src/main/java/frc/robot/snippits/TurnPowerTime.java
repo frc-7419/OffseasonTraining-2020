08f28a9e86f0b2ab25d7416d2438bbf66e9c9725
braden0m <95321149+braden0m@users.noreply.github.com>
2022-01-28 00:08:49 -0800

    Week 9

--- src/main/java/frc/robot/snippits/TurnPowerTime.java
+++ src/main/java/frc/robot/snippits/TurnPowerTime.java
@@ -1,31 +1,55 @@
 package frc.robot.snippits;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivebase.DriveBaseSub;
 
 public class TurnPowerTime extends CommandBase {
-  
-  public TurnPowerTime() {
+  private DriveBaseSub driveBase;
+  private double power;
+  private double time;
+  private String direction;
+  private double initialTime = System.currentTimeMillis();
+
+  public TurnPowerTime(DriveBaseSub driveBase, double power, double time, String direction) {
+    this.driveBase = driveBase;
+    this.power = power;
+    this.time = time;
+    this.direction = direction;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveBase);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    driveBase.coast();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    if (direction == "Right") {
+      driveBase.setRightPower(-power);
+      driveBase.setLeftPower(power);
+    }
+    else {
+      driveBase.setRightPower(power);
+      driveBase.setLeftPower(-power);
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    driveBase.setPower(0.0);
+    driveBase.brake();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    double currentTime = System.currentTimeMillis();
+    if (currentTime - initialTime > time) return true;
+    else return false;
   }
 }
