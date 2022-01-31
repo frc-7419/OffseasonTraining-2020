13eedd2fcf77bc1372b2f713e494f584bc146e0a
Siddharth Ramalingam <siddharthramalingam06@gmail.com>
2022-01-30 20:40:45 -0800

    Week 4 Assignment

--- src/main/java/frc/robot/subsystems/intake/RunIntake.java
+++ src/main/java/frc/robot/subsystems/intake/RunIntake.java
@@ -1,13 +1,16 @@
-  // Called every time the scheduler runs while the command is scheduled.
+
   @Override
+
   public void execute() {
+
     intake1.setPower(power);
+
   }
 
-  // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    intake1.setPower(0);
-  }
 
-  // Returns true when the command should end.
+  public void end(boolean interrupted) {
+
+    intake1.setPower(0);
+
+  }
