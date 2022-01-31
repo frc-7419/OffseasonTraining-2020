2489740fd5edfd60593af884f98c3b9f76a29e76
Siddharth Ramalingam <siddharthramalingam06@gmail.com>
2022-01-30 20:53:13 -0800

    Week 5 Assignment

--- src/main/java/frc/robot/subsystems/intake/RunIntake.java
+++ src/main/java/frc/robot/subsystems/intake/RunIntake.java
@@ -1,16 +1,49 @@
+package frc.robot.subsystems.intake;
 
-  @Override
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
-  public void execute() {
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-    intake1.setPower(power);
+import com.team7419.Initers;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+
+public class IntakeSub extends SubsystemBase {
+
+   private VictorSPX victor1;
+
+  public IntakeSub(VictorSPX victor) {
+
+    this.victor1 = victor;
+
+    Initers.initVictors(victor);
+
+    victor.setInverted(false);
+  }
+
+  public VictorSPX getVictor(){
+
+    return victor1;
+  
+  }
+
+  public void setPower(double p){
+
+    victor1.set(ControlMode.PercentOutput, p);
 
   }
 
+  public boolean getInverted(){
+
+    return victor1.getInverted();
+  }
+
   @Override
-
-  public void end(boolean interrupted) {
-
-    intake1.setPower(0);
+  
+  public void periodic() {
 
   }
+}
