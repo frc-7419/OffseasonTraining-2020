cee330272c4f4c45d6ca447608ee499709d1e3f6
Siddharth Ramalingam <siddharthramalingam06@gmail.com>
2022-01-30 20:55:28 -0800

    Week 6 Assignment

--- src/main/java/frc/robot/subsystems/drivebase/ArcadeDrive.java
+++ src/main/java/frc/robot/subsystems/drivebase/ArcadeDrive.java
@@ -5,27 +5,37 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class ArcadeDrive extends CommandBase {
   
   
-  /**
-   * Creates a new ArcadeDrive.
-   */
+
   public ArcadeDrive() {
+
   }
 
   @Override
+
   public void initialize() {
+
   }
 
+
   @Override
+
   public void execute() {
+
   }
 
   @Override
+
   public void end(boolean interrupted) {
+
   }
 
+
   @Override
+
   public boolean isFinished() {
+
     return false;
+    
   }
   
 }
--- src/main/java/frc/robot/subsystems/drivebase/DriveBaseSub.java
+++ src/main/java/frc/robot/subsystems/drivebase/DriveBaseSub.java
@@ -5,14 +5,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveBaseSub extends SubsystemBase {
   
   
-  /**
-   * Creates a new DriveBaseSub.
-   */
+  
+  
   public DriveBaseSub() {
+
   }
 
   @Override
+
   public void periodic() {
+    
   }
 
 }
